f6be1b4f0e5eb791aad9bd1701e840f86b95d7e0
superwarrior786 <elkhai@ymail.com>
2024-03-07 23:34:49 -0500

    Seventh Commit
    completed most of the UI for the homepage

--- bank-app/src/Homepage.js
+++ bank-app/src/Homepage.js
@@ -3,10 +3,10 @@ import React from "react";
 export default function Homepage() {
   return (
     <div
-      className="bg-zinc-600 w-10/12 h-5/6 col-span-9 ml-20 rounded-[50px] border-2 border-orange-400 
+      className="bg-zinc-600 w-10/12 h-5/6 col-span-9 ml-20 rounded-[50px] border-2 border-orange-400 border-opacity-20 
     shadow-[0_35px_60px_-15px_rgba(251,146,60,0.4)] grid grid-row-8 grid-cols-1"
     >
-      <div className="w-full h-24 overflow-hidden flex items-center justify-between row-span-1 -mb-12">
+      <div className="w-full h-24 overflow-hidden flex items-center justify-between row-span-1">
         <p className="font-extrabold text-5xl text-orange-400 ml-10">
           Overview
         </p>
@@ -15,32 +15,100 @@ export default function Homepage() {
         </p>
       </div>
       <div className="grid grid-cols-12 grid-rows-12 grid-flow-col row-span-7 ml-5 mr-5 mb-5 gap-y-3 gap-x-4">
-        <div className="col-span-3 row-span-6 rounded-[50px] bg-zinc-500 flex">
+        <div className="col-span-3 row-span-6 rounded-[50px] bg-zinc-500 flex flex-col gap-y-10">
           <p className="text-2xl font-bold ml-6 mt-7 text-orange-200">
             My Cards
           </p>
+          <div className="bg-gradient-to-tl from-orange-400 to-red-400 w-10/12 h-3/6 self-center rounded-xl flex flex-col">
+            <p className="font-bold ml-5 mt-16 self-center">
+              **** **** **** 4321
+            </p>
+            <p className="font-bold text-xl ml-5 mr-5 whitespace-nowrap line-clamp-1">
+              Elkhai Nagawkar
+            </p>
+            <p className="font-semibold  text-xs ml-5">01/01</p>
+          </div>
         </div>
-        <div className="col-span-3 row-span-6 rounded-[50px] bg-zinc-500"></div>
+        <div className="col-span-3 row-span-6 rounded-[50px] bg-zinc-500 flex flex-col justify-evenly">
+          <div>
+            <p className="text-5xl text-green-500 font-bold ml-5 text-wrap">
+              $400,000
+            </p>
+            <p className="font-bold text-xl ml-5">Your Money</p>
+          </div>
+          <div>
+            <p className="text-5xl text-red-500 font-bold ml-5">$400,000</p>
+            <p className="font-bold text-xl ml-5">Loan amount</p>
+          </div>
+        </div>
         <div className="col-span-6 row-span-12 rounded-[50px] bg-zinc-500">
           <p className="text-4xl font-bold ml-10 mt-7 text-orange-200">
             Transactions
           </p>
         </div>
-        <div className="col-span-3 row-span-6 rounded-[50px] bg-zinc-500 flex flex-col">
-          <p className="text-2xl font-bold ml-6 mt-7 mb-5 text-orange-200">
-            Deposit
-          </p>
-          <p className="text-2xl font-bold ml-6 mt-7 text-orange-200">
-            Add Expense
-          </p>
+        <div className="col-span-3 row-span-6 rounded-[50px] bg-zinc-500 flex flex-col justify-between">
+          <div className="grid grid-cols-5 grid-rows-2 items-center mt-5">
+            <p className="text-2xl font-bold ml-6 mt-7 mb-1 text-orange-200 col-span-5">
+              Deposit
+            </p>
+            <input
+              type="text"
+              placeholder="Amount"
+              className="text-white placeholder:text-white placeholder:text-opacity-30 rounded-full w-full h-4/6 ml-5 bg-zinc-600 border-2 border-zinc-400 focus:outline-none indent-5 col-span-3"
+            />
+            <button className="col-span-2 col-start-5 w-8/12 h-4/6 bg-gradient-to-tr from-green-500 to-teal-500 rounded-full text-xl font-extrabold">
+              ➞
+            </button>
+          </div>
+          <div className="grid grid-cols-5 grid-rows-2 items-center mb-10">
+            <p className="text-2xl font-bold ml-6 mt-7 mb-1 text-orange-200 col-span-5">
+              Add Expense
+            </p>
+            <input
+              type="text"
+              placeholder="Amount"
+              className="text-white placeholder:text-white placeholder:text-opacity-30 rounded-full w-full h-4/6 ml-5 bg-zinc-600 border-2 border-zinc-400 focus:outline-none indent-5 col-span-3"
+            />
+            <button className="col-span-2 col-start-5 w-8/12 h-4/6 bg-gradient-to-tr from-rose-400 to-rose-600 rounded-full text-xl font-extrabold">
+              ➞
+            </button>
+          </div>
         </div>
         <div className="col-span-3 row-span-3 rounded-[50px] bg-zinc-500">
-          <p className="text-2xl font-bold ml-6 mt-7 text-orange-200">
-            Send Money
-          </p>
+          <div className="grid grid-cols-6 items-center gap-x-5">
+            <p className="text-2xl font-bold ml-6 mt-7 mb-3 text-orange-200 col-span-6">
+              Send Money
+            </p>
+            <input
+              type="text"
+              placeholder="Amount"
+              className="col-span-3 w-10/12 h-8 ml-7 rounded-full text-white placeholder:text-white placeholder:text-opacity-30 bg-zinc-600 border-2 border-zinc-400 focus:outline-none indent-3"
+            />
+            <input
+              type="text"
+              placeholder="Reciever name"
+              className="col-span-3 w-10/12 h-8 rounded-full text-white placeholder:text-white placeholder:text-opacity-30 bg-zinc-600 border-2 border-zinc-400 focus:outline-none indent-3"
+            />
+            <button className="col-span-2 col-start-3 bg-green-400 rounded-full mt-3 h-5/6 font-bold ">
+              Send
+            </button>
+          </div>
         </div>
         <div className="col-span-3 row-span-3 rounded-[50px] bg-zinc-500">
-          <p className="text-2xl font-bold ml-6 mt-7 text-orange-200">Loan</p>
+          <div className="grid grid-cols-5 items-center gap-x-5">
+            <p className="text-2xl font-bold ml-6 mt-7 mb-6 text-orange-200 col-span-6">
+              Loan
+            </p>
+            <input
+              type="text"
+              placeholder="Amount"
+              className="col-span-3 w-10/12 h-8 ml-7 rounded-full text-white placeholder:text-white placeholder:text-opacity-30 bg-zinc-600 border-2 border-zinc-400 focus:outline-none indent-3"
+            />
+
+            <button className="col-span-2 col-start-4 bg-green-400 rounded-full h-full font-bold ">
+              Request
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- bank-app/src/Navbar.js
+++ bank-app/src/Navbar.js
@@ -5,7 +5,7 @@ import { VscAccount } from "react-icons/vsc";
 
 export default function Navbar() {
   return (
-    <div className="group flex w-24 h-72 rounded-e-[50px] bg-zinc-600 border-2 border-s-transparent border-e-orange-400 border-t-orange-400 border-b-orange-400 hover:w-56 transition-all col-span-1 shadow-[0_8px_30px_rgb(251,146,60,0.16)]">
+    <div className="group flex w-24 h-72 rounded-e-[50px] bg-zinc-600 border-2 border-s-transparent border-e-orange-400 border-t-orange-400 border-b-orange-400 border-opacity-40 hover:w-56 transition-all col-span-1 shadow-[0_8px_30px_rgb(251,146,60,0.16)]">
       <div className="w-full h-full flex flex-col justify-evenly items-center">
         <div className="w-full flex items-center justify-center gap-x-3">
           <TiHome className="w-10 h-20" />
